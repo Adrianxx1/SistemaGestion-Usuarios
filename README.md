a1ae60b265f4db1256f46c4567b81d8236f11f67
Adrianxx1 <triaxis12q@gmail.com>
2025-07-27 06:06:36 -0600

    Generando getters and setters

---
+++ README.md
@@ -0,0 +1,2 @@
+# SistemaGestion-Usuarios
+En este proyecto estare realizando un sistema que me permita gestionar usuarios existentes, sera un proyecto full stack usando las tecnologias de JAVA, SpringBoot, Hibernate, JWT y Api Rest
--- curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,14 +1,47 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Luis");
+        usuario.setApellido("Amador");
+        usuario.setTelefono("2413222234");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario23")
+    public Usuario editar(){
+     Usuario usuario = new Usuario();
+     usuario.setNombre("Luis");
+     usuario.setApellido("Amador");
+     usuario.setTelefono("2413222234");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario233")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Luis");
+        usuario.setApellido("Amador");
+        usuario.setTelefono("2413222234");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario12")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Luis");
+        usuario.setApellido("Amador");
+        usuario.setTelefono("2413222234");
+        return usuario;
     }
 }
 
